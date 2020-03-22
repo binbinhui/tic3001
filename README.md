a3b82f8e04a949de8672a58cb348d57b42613692
binhui <44106725+binbinhui@users.noreply.github.com>
2020-03-22 11:43:22 +0800

    Dependency change

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tic3001
+
+1. Set C:\..\TIC3001 Assignment 2 as the Content Root.
+
+2. Set C:\..\TIC3001 Assignment 2\A0177740Y_A0177820B\src as the Source Folder.
+
+3. Ensure that the TIC3001-Assignment2-testcases are in the C:\..\TIC3001 Assignment 2 folder.
+
+4. Run Main in your IDE, then type 1 to 5 and hit Enter to do the respective actions.
+
+KWIC Circulator 2.png shows how the program looks like.
--- src/test/java/sg/edu/nus/comp/tic3001/kwic_assignment2/ExtensionsTest.java
+++ src/test/java/sg/edu/nus/comp/tic3001/kwic_assignment2/ExtensionsTest.java
@@ -3,24 +3,28 @@ package sg.edu.nus.comp.tic3001.kwic_assignment2;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class ExtensionsTest extends TestCase {
 	
+	@Test
 	public void test() {
 		
 		Extensions exten = new Extensions();
 		List<String> sup = Arrays.asList("sup1", "sup2", "sup3");
 		boolean output = exten.checkIfFileIsEmpty(true, sup, "hihi");
-		System.out.print(output);
+		//System.out.print(output);
 		assertEquals(false, output);
-	}     
+	}  
+	@Test
 	public void test2() {
 		
 		Extensions exten = new Extensions();
 		List<String> sup = Arrays.asList("sup1", "sup2", "sup3");
 		boolean output = exten.checkIfFileIsEmpty(sup, "haha", "hihi");
-		System.out.print(output);
+		//System.out.print(output);
 		assertEquals(false, output);
 	}
 
